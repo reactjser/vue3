567b2b4dfb5b4a35e1947b98f3981c9ce8302a62
liuyao18 <liuyao18@baijia.com>
2021-07-17 17:48:24 +0800

    chore: add bundle analyse

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,48 @@
 import { join } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfigExport } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import styleImport from 'vite-plugin-style-import';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig({
-    plugins: [
-        vue(),
-        styleImport({
-            libs: [
-                {
-                    libraryName: 'element-plus',
-                    esModule: true,
-                    ensureStyleFile: true,
-                    resolveStyle: (name) => {
-                        name = name.slice(3);
-                        return `element-plus/packages/theme-chalk/src/${name}.scss`;
-                    },
-                    resolveComponent: (name) => {
-                        return `element-plus/lib/${name}`;
-                    },
-                },
-            ],
-        }),
-    ],
-    resolve: {
-        alias: {
-            '/@': join(__dirname, 'src'),
+type Mode = 'development' | 'test' | 'production';
+
+export default ({ mode }: { mode: Mode }): UserConfigExport =>
+    defineConfig({
+        build: {
+            rollupOptions: {
+                plugins: [
+                    mode === 'test' &&
+                        visualizer({
+                            open: true,
+                        }),
+                ],
+            },
         },
-    },
-    server: {
-        port: 8000,
-    },
-});
+        plugins: [
+            vue(),
+            styleImport({
+                libs: [
+                    {
+                        libraryName: 'element-plus',
+                        esModule: true,
+                        ensureStyleFile: true,
+                        resolveStyle: (name) => {
+                            name = name.slice(3);
+                            return `element-plus/packages/theme-chalk/src/${name}.scss`;
+                        },
+                        resolveComponent: (name) => {
+                            return `element-plus/lib/${name}`;
+                        },
+                    },
+                ],
+            }),
+        ],
+        resolve: {
+            alias: {
+                '/@': join(__dirname, 'src'),
+            },
+        },
+        server: {
+            port: 8000,
+        },
+    });
