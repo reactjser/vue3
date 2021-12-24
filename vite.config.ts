b36f7fe2843a29581a731fd834bba5ffd89fa728
reactjser <reactjser@163.com>
2021-12-24 17:45:29 +0800

    fix: build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,10 @@
-import { join, resolve } from 'path';
-import { readFileSync } from 'fs';
+import { join } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import lessToJS from 'less-vars-to-js';
 import styleImport, { VantResolve } from 'vite-plugin-style-import';
-
-const themeVariables = lessToJS(
-  readFileSync(resolve(__dirname, './src/styles/antd.customize.less'), 'utf8'),
-);
 
 // https://cn.vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        modifyVars: themeVariables,
-      },
-    },
-  },
   plugins: [
     vue(),
     styleImport({
